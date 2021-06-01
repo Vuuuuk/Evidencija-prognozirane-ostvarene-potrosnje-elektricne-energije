fd749b23e7a13c9f3d2c72f9d85a3a72d2d17a80
Milan Stevanovic <milanste999@gmail.com>
2021-06-01 20:21:08 +0200

    Dodat BackUp baze podataka

---
+++ MS Baza BackUp/baza_backup_sql.sql
@@ -0,0 +1,31 @@
+CREATE TABLE EvidencijaNevalidnihFajlova(
+    vreme varchar(255),
+    imeFajla varchar(255),
+    lokacija varchar(255),
+    brojRedova int
+);
+
+CREATE TABLE EvidencijaOstvarenePotrosnje(
+    vremeUcitavanja varchar(10),
+    imeFajla varchar(255),
+    lokacija varchar(255),
+    sat int,
+    load int,
+    oblast varchar(10),
+    datum date
+);
+
+CREATE TABLE EvidencijaPrognoziranePotrosnje(
+    vremeUcitavanja varchar(10),
+    imeFajla varchar(255),
+    lokacija varchar(255),
+    sat int,
+    load int,
+    oblast varchar(10),
+    datum date
+);
+
+CREATE TABLE EvidencijaGeoOblasti(
+    naziv varchar(10),
+    sifra varchar(10)
+)
